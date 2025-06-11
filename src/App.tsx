3e31b96e8ae53fc0e14b35ccf04af7eaf3b0e26d
ssulochanaherath <ssulochanaherath@gmail.com>
2025-06-11 22:43:19 +0530

    Started making website

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing.tsx';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+            </Routes>
+        </Router>
+    )
+}
